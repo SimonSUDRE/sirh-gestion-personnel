97a7e84ba4fcede91a717f117a8775ef6fe448c5
SimonSUDRE <sta.sudre@gmail.com>
2018-01-29 15:11:12 +0100

    tag v3

--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -1,6 +1,7 @@
 package dev.sgp.web;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,19 +18,23 @@ public class ListerCollaborateursController extends HttpServlet {
 	 */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		// recupere la valeur d'un parametre dont le nom est avecPhoto
-		String avecPhotoParam = req.getParameter("avecPhoto");
-		
-		// recupere la valeur d'un parametre dont le nom est departement
-		String departementParam = req.getParameter("departement");
-		resp.setContentType("text/html");
-		
-		// code HTML ecrit dans le corps de la reponse
-		resp.getWriter().write("<h1>Liste des collaborateurs</h1>"
-		+ "<ul>"
-		+ "<li>avecPhoto="+ avecPhotoParam + "</li>"
-		+ "<li>departement="+ departementParam + "</li>"
-		+ "</ul>");
+		try {
+			req.setAttribute("listeNoms", Arrays.asList("Robert", "Jean", "Hugues"));
+			req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp").forward(req, resp);
+		} catch(ServletException | IOException e) {
+			e.getMessage();
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			doGet(req, resp);
+		} catch(ServletException | IOException e) {
+			e.getMessage();
+		}
 	}
 }
